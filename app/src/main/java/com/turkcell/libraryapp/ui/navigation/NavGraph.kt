ddce60b3d5fe9888a033e1448da5f2ed87688ec6
ÇAĞLA BEYZA KANDEMİR <caglakandemir8@gmail.com>
2026-04-28 02:33:27 +0300

    Proje altyapısı ve Supabase bağlantısı düzeltildi

---
+++ app/src/main/java/com/turkcell/libraryapp/ui/navigation/NavGraph.kt
@@ -0,0 +1,19 @@
+package com.turkcell.libraryapp.ui.navigation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.halit.ui.screen.auth.RegisterScreen
+import com.turkcell.libraryapp.ui.screen.LoginScreen
+
+@Composable
+fun NavGraph(navController: NavHostController = rememberNavController()) {
+    NavHost(navController = navController, startDestination = Screen.Register.route)
+    {
+        composable(Screen.Login.route) { LoginScreen() }
+        composable(Screen.Register.route) { RegisterScreen() }
+    }
+}
