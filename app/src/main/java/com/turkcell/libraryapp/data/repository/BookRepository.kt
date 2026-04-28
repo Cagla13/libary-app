53f7bf6fd99eca0fc3c5e094d848143c30b9715d
Cagla13 <senin_epostan@gmail.com>
2026-04-29 00:48:36 +0300

    Ödev 2: BookRepository güncelleme, silme ve arama fonksiyonları eklendi

--- app/src/main/java/com/turkcell/libraryapp/data/repository/BookRepository.kt
+++ app/src/main/java/com/turkcell/libraryapp/data/repository/BookRepository.kt
@@ -5,21 +5,53 @@ import com.turkcell.libraryapp.data.supabase.supabase
 import io.github.jan.supabase.postgrest.postgrest
 
 class BookRepository {
+
     suspend fun getAllBooks(): Result<List<Book>> = runCatching {
         supabase.postgrest["books"]
             .select()
             .decodeList<Book>()
     }
 
-    suspend fun getBookById(id:String): Result<Book> = runCatching {
+
+    suspend fun getBookById(id: String): Result<Book> = runCatching {
         supabase.postgrest["books"]
-            .select { filter { eq("id",id) } }
+            .select {
+                filter { eq("id", id) }
+            }
             .decodeSingle<Book>()
     }
+
 
     suspend fun addBook(book: Book): Result<Unit> = runCatching {
         supabase.postgrest["books"].insert(book)
     }
 
-    // ÖDEV 2: BookRepository Güncelleme, silme, arama fonksiyonlarını tanımla.
+
+
+
+    suspend fun updateBook(id: String, updatedBook: Book): Result<Unit> = runCatching {
+        supabase.postgrest["books"].update(updatedBook) {
+            filter { eq("id", id) }
+        }
+    }
+
+
+    suspend fun deleteBook(id: String): Result<Unit> = runCatching {
+        supabase.postgrest["books"].delete {
+            filter { eq("id", id) }
+        }
+    }
+
+
+    suspend fun searchBooks(query: String): Result<List<Book>> = runCatching {
+        supabase.postgrest["books"]
+            .select {
+                filter {
+                    ilike("title", "%$query%")
+                }
+            }
+            .decodeList<Book>()
+    }
+
+
 }
